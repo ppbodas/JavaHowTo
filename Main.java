f7b4d1f3f53a24e8d2f8e871a4e38c3bda2ef0c4
Prat <ppbodas@users.noreply.github.com>
2023-04-19 19:42:31 -0700

    Update Main.java
    Split in multiple private functions

--- Main.java
+++ Main.java
@@ -12,24 +12,25 @@ public class Main {
     }
 
     public void runPrograms() {
+        // Print Java Integer limiting values
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+
         // Read file from console
         // readDataFromConsoleLineByLine();
 
-        // Define Array
-        int [] intArr = {1, 3, 5};
-        Arrays.stream(intArr).forEach(System.out::println);
+        arrayHowTos();
 
-        int [] intArr1 = new int[5];
-        intArr1[3] = 8;
-        Arrays.stream(intArr1).forEach(System.out::println);
+        arrayListHowTos();
 
-        // Define ArrayList
-        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3));
-        intList.forEach(System.out::print);
+        hashMapHowTos();
 
-        System.out.println();
+    }
 
-        // Initialize map
+    private static void hashMapHowTos() {
+        System.out.println("------------ hashMap How Tos ----------------");
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.put("Prathmesh", 100);
         hashMap.put("Anay", 100);
@@ -37,19 +38,44 @@ public class Main {
             System.out.println("Key is: " + k + " Value is: " + v.toString());
         });
 
+        System.out.println("");
+    }
+
+    private static void arrayListHowTos() {
+        System.out.println("------------ arrayList How Tos ----------------");
+        // Define ArrayList
+        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3));
+        intList.forEach(System.out::print);
+
+        System.out.println();
+
         // iterating over arraylist
         ArrayList<Integer> intList1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         for (Integer i : intList1) {
-            System.out.println(i);
+            System.out.print(i);
         }
 
-        // Print Java Integer limiting values
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Double.MAX_VALUE);
-        System.out.println(Double.MIN_VALUE);
+        // Change second element
+        intList1.set(1, 5);
+        intList1.forEach(System.out::print);
 
+        System.out.println("");
+    }
 
+    private static void arrayHowTos() {
+        System.out.println("------------ array How Tos ----------------");
+        // Define Array
+        int [] intArr = {1, 3, 5};
+        Arrays.stream(intArr).forEach(System.out::println);
+
+        for (int i = 0; i < intArr.length; ++i) {
+            System.out.print(intArr[i]);
+        }
+        System.out.println("");
+
+        int [] intArr1 = new int[5];
+        intArr1[3] = 8;
+        Arrays.stream(intArr1).forEach(System.out::println);
     }
 
     private void readDataFromConsoleLineByLine() {
